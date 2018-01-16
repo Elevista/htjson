b874a0581450d53b076bf85f4c526715ec7fc8fe
Elevista <sunnyholic@sunnyholic.com>
2018-01-16 11:22:38 +0900

    init

---
+++ index.js
@@ -0,0 +1,86 @@
+let _indent = '  '
+
+function arrayToMap (arr, fn) {
+  let ret = {}
+  arr.forEach((v, k) => { ret[v] = fn(v, k) })
+  return ret
+}
+
+let inlineTags = arrayToMap(['p', 'h1', 'h2', 'h3', 'h4', 'span', 'a', '$text', 'br'], x => true)
+
+function judgeInline (obj) {
+  if (obj instanceof Array) return true
+  else if (obj instanceof Object) {
+    let childLen = Object.keys(obj).filter(x => /^[^$_]/.test(x)).length
+    if (!childLen) return true
+    else if (childLen === 1) return !obj.$text
+    return false
+  } else return true
+}
+
+function normalizeChildren (node) {
+  if (node instanceof Object) {
+    let $tag = Object.keys(node).shift()
+    if (node[$tag] instanceof Object) {
+      if (node[$tag] instanceof Array) return normalize(node[$tag].slice(), $tag)
+      else return normalize(Object.assign({}, node[$tag]), $tag)
+    } else return normalize(node[$tag], $tag)
+  } else return normalize(node, '$text')
+}
+
+function normalize (obj, $tag = '') {
+  let $inline = judgeInline(obj, $tag) && !!inlineTags[$tag]
+  if (!(obj instanceof Object)) obj = {$tag, $attrs: [], $text: obj}
+  if (obj instanceof Array) obj = {$tag, $attrs: [], $text: '', $: obj}
+  if ($tag === '$text') return {$tag, $text: obj.$text}
+  let ret = {$tag, $attrs: [], $children: [], $inline}
+  for (let key of Object.keys(obj)) {
+    let value = obj[key]
+    switch (key[0]) {
+      case '$':
+        if (key.length === 1) {
+          ret.$children.push(...obj.$.map(x => normalizeChildren(x)))
+        }
+        if ($tag !== '$text' && key === '$text') ret.$children.push({$tag: '$text', $text: value})
+        break
+      case '_':
+        ret.$attrs.push(` ${key.slice(1)}="${value}"`)
+        break
+      default:
+        ret.$children.push(normalize(value, key))
+    }
+  }
+  return ret
+}
+
+function compile (obj) {
+  let {$tag: tag, $attrs: attrs, $text: text = '', $inline: inline, $children: children} = obj
+  if (tag === '$text') return text
+  if (inline) return `<${tag}${attrs.join('')}>${text || children.map(x => compile(x)).join('')}</${tag}>`
+  let block = []
+  if (tag) block.push(`<${tag}${attrs.join('')}>`)
+  block.push(...children.map(x => compile(x)))
+  if (tag) block.push(`</${tag}>`)
+  return block
+}
+
+function indenetTree (arr, depth = 0) {
+  let indent = _indent.repeat(Math.max(depth, 0))
+  return arr.map(x => x instanceof Array ? indenetTree(x, depth + 1) : indent + x).join('\n')
+}
+
+const htjson = {
+  compile (obj) {
+    let normal = normalize(obj)
+    let htmlTree = compile(normal)
+    return indenetTree(htmlTree, -1)
+  }
+}
+Object.defineProperties(htjson, {
+  indent: {
+    set (v) { _indent = v },
+    get () { return _indent }
+  }
+})
+
+module.exports = module.exports.htjson = htjson
---
+++ test.js
@@ -0,0 +1,106 @@
+const htjson = require('./index')
+
+let input = `
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+div.container {
+    width: 100%;
+    border: 1px solid gray;
+}
+</style>
+</head>
+<body>
+
+<div class="container">
+
+  <header>
+     <h1>City Gallery</h1>
+  </header>
+    
+  <nav>
+    <ul class="abc">
+      <li><a href="#">London</a></li>
+      <li><a href="#">Paris</a></li>
+      <li><a href="#">Tokyo</a></li>
+    </ul>
+  </nav>
+  
+  <article>
+    <h1>London</h1>
+    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+    <p>abcd<br>efgh</p>
+    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+  </article>
+  
+  <footer>Copyright &copy; W3Schools.com</footer>
+  
+  </div>
+
+</body>
+</html>`
+
+let json = {
+  html: {
+    head: {
+      style: `
+      div.container {
+        width: 100%;
+        border: 1px solid gray;
+      }`
+    },
+    body: {
+      div: {
+        _class: 'container',
+        header: {
+          h1: 'City Gallery'
+        },
+        nav: {
+          ul: {
+            _class: 'abc',
+            $: [
+              {li: {a: {_href: '#', $text: 'london'}}},
+              {li: {a: {_href: '#', $text: 'Paris'}}},
+              {li: {a: {_href: '#', $text: 'Tokyo'}}}
+            ]
+          }
+        },
+        article: [
+          {h1: 'london'},
+          {p: 'London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.'},
+          {p: ['abcd', {br: '/'}, 'efgh']},
+          {p: 'Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.'}
+        ],
+        footer: 'Copyright &copy; W3Schools.com'
+      }
+    }
+  }
+}
+console.log(htjson.compile(json))
+// console.log(htjson.compile({p: ['abcd', {br: '/'}, 'efgh']}))
+
+// console.log(htjson.compile({
+//   article: [
+//     {p: ['abcd', {br: '/'}, 'efgh']},
+//   ],
+// }))
+// htjson.compile({
+//   article: [
+//     // {h1: 'london'},
+//     // {p: 'London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.'},
+//     {p: ['abcd', {br: '/'}, 'efgh']},
+//     // {p: 'Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.'}
+//   ],
+// })
+// htjson.compile({
+//   ul: {
+//     _class: 'abc',
+//     $: [
+//       {li: {a: {_href: '#', $text: 'london'}}},
+//       {li: {a: {_href: '#', $text: 'Paris'}}},
+//       {li: {a: {_href: '#', $text: 'Tokyo'}}}
+//     ]
+//   }
+// })
+//
